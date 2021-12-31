e23286cf1c62bb7f25e47c95161ed80659364811
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-31 14:47:34 -0600

    added info to readme file and adding to js file

---
+++ assets/javascript/script.js
@@ -0,0 +1,5 @@
+function initPage() {
+    var cityEl = document.getElementById("enter-city");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+}
