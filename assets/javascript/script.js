e5850ec59c6f9fe0212a5345529054584a75ff85
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-31 15:08:18 -0600

    adding variables to js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,4 +2,9 @@ function initPage() {
     var cityEl = document.getElementById("enter-city");
     var searchEl = document.getElementById("search-button");
     var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");
+    var currentWindEl = document.getElementById("wind-speed");
 }
