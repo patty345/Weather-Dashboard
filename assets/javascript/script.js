c21bde45bf85b23ae028749dd5dd26e9a85b82fe
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-01-06 20:17:18 -0600

    fixed js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -123,7 +123,7 @@ var displayUvIndex = function(index){
 }
 
 var get5Day = function(city){
-    var apiKey = "844421298d794574c100e3409cee0499"
+    var apiKey = "542892d5f29e2b33c206cb20ad5b5de5"
     var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
     fetch(apiURL)
